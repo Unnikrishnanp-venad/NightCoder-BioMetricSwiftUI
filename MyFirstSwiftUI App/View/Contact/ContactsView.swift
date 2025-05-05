797bf7e02a575931059c47cf26d3ca60ef27af6e
Unnikrishnan <unnikrishnanpvenad@gmail.com>
2025-05-05 16:45:51 +0530

    Updated

--- MyFirstSwiftUI App/View/Contact/ContactsView.swift
+++ MyFirstSwiftUI App/View/Contact/ContactsView.swift
@@ -10,7 +10,7 @@ import Contacts
 struct ContactsView: View {
     var body: some View {
         VStack {
-                   TableView()
-               }
+            TableView()
+        }
     }
 }
