92ca056908a103836eca389608b146a0caf1112a
Unnikrishnan <unnikrishnanpvenad@gmail.com>
2025-05-05 13:36:22 +0530

    Updated

--- MyFirstSwiftUI App/View/Tab/MainTabView.swift
+++ MyFirstSwiftUI App/View/Tab/MainTabView.swift
@@ -9,26 +9,41 @@ import Foundation
 import SwiftUI
 
 struct MainTabView: View {
+    
+    @State var presentSideMenu = false
+    @State var selectedSideMenuTab = 0
     var body: some View {
-        TabView {
-           
-            RegistrationView()
-                .tabItem {
-                    Label("Home", systemImage: "house")
-                }
-            ContactsView()
-                .tabItem {
-                    Label("Search", systemImage: "magnifyingglass")
-                }
-            GalleryGridView()
-                .tabItem {
-                    Label("Gallery", systemImage: "photo")
-                }
-            LandingView()
-                .tabItem {
-                    Label("Settings", systemImage: "gearshape")
-                }
-
-        }.navigationBarBackButtonHidden(true) // Hide back button
+        NavigationStack {
+            ZStack{
+                TabView {
+                    RegistrationView()
+                        .tabItem {
+                            Label("Home", systemImage: "house")
+                        }
+                    ContactsView()
+                        .tabItem {
+                            Label("Search", systemImage: "magnifyingglass")
+                        }
+                    GalleryGridView()
+                        .tabItem {
+                            Label("Gallery", systemImage: "photo")
+                        }
+                    LandingView()
+                        .tabItem {
+                            Label("Settings", systemImage: "gearshape")
+                        }
+                    
+                } .navigationBarItems(leading: (
+                    Button(action: {
+                        
+                        presentSideMenu =  presentSideMenu ? false : true
+                    }) {
+                        Image(systemName: "line.horizontal.3")
+                            .imageScale(.large)
+                    }))
+                SideMenu(isShowing: $presentSideMenu, content: AnyView(SideMenuView(selectedSideMenuTab: $selectedSideMenuTab, presentSideMenu: $presentSideMenu)))
+            }
+        }
     }
+    
 }
